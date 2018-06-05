b9c5f84dda8947b6568fb0e56b9f8ea113f4c10e
vagrant <vagrant@xubuntu>
2018-06-05 12:48:25 +0000

    vagranfile with proxy provisioning

---
+++ vm-xubuntu/provision.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function start_cntlm() {
+  service cntlm start;
+  local running="";
+  while [ -z "${running}" ]; do
+    sleep 1;
+    running=$(service cntlm status | grep 'active (running)');
+  done
+}
+
+function main() {
+  local -r http_proxy="${1:?[ERROR] http_proxy not provided (http://<domain>\<username>:<password>@<proxy>:<port>)}";
+  pushd /vagrant/toolbox
+  ./configure-proxy "${http_proxy}"
+  apt-get update && apt-get install -y cntlm
+  ./configure-cntlm "${http_proxy}"
+  ./configure-proxy "http://127.0.0.1:3128"
+  start_cntlm
+  ./configure-apt
+  ./install-packet-installation-tools 
+  #./install-packet-dev "vagrant"
+  popd;
+}
+
+main "$@"
