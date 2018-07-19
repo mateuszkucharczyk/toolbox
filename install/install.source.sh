67c563dad0182248c31ffd8e7f7a8feb1daa88e1
mateuszkucharczyk <mateuszkucharczyk@protonmail.com>
2018-07-19 12:42:35 +0000

    script print stacktrace on error

--- install/install.source.sh
+++ install/install.source.sh
@@ -4,6 +4,8 @@ source ../bin/strings.sh
 
 function echoerr() { 
   echo "$@" 1>&2; 
+  echo "  in function: ${FUNCNAME[1]}" 1>&2;
+  echo "    stacktrace: ${FUNCNAME[*]}" 1>&2;
 }
 
 #https://stackoverflow.com/a/5300429
